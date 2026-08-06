02b071a9f14fb23f895cc5b1fbc603a098811730
anshuonline <helloanshu.dev@gmail.com>
2026-08-07 02:41:59 +0530

    Fix Tawk.to script insertion method

--- components/footer.php
+++ components/footer.php
@@ -97,17 +97,20 @@
         }, { threshold: 0.1 });
         document.querySelectorAll('.fade-up').forEach(el => observer.observe(el));
     </script>
-    
     <!--Start of Tawk.to Script-->
     <script type="text/javascript">
     var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
     (function(){
-    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-    s1.async=true;
-    s1.src='https://embed.tawk.to/6a74f49904e09a1d44f872f7/1jvcdmu2l';
-    s1.charset='UTF-8';
-    s1.setAttribute('crossorigin','*');
-    s0.parentNode.insertBefore(s1,s0);
+        try {
+            var s1=document.createElement("script");
+            s1.async=true;
+            s1.src='https://embed.tawk.to/6a74f49904e09a1d44f872f7/1jvcdmu2l';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            document.body.appendChild(s1);
+        } catch (e) {
+            console.error("Tawk.to injection error:", e);
+        }
     })();
     </script>
     <!--End of Tawk.to Script-->
