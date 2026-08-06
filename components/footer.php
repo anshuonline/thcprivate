da5a5dd36a4c0e6e1eabbf9abd265b4bf5a705b9
anshuonline <helloanshu.dev@gmail.com>
2026-08-07 03:21:27 +0530

    Revert Tawk script to exact original

--- components/footer.php
+++ components/footer.php
@@ -101,16 +101,12 @@
     <script type="text/javascript">
     var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
     (function(){
-        try {
-            var s1=document.createElement("script");
-            s1.async=true;
-            s1.src='https://embed.tawk.to/6a74f49904e09a1d44f872f7/1jvcdmu2l';
-            s1.charset='UTF-8';
-            s1.setAttribute('crossorigin','*');
-            document.body.appendChild(s1);
-        } catch (e) {
-            console.error("Tawk.to injection error:", e);
-        }
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/6a74f49904e09a1d44f872f7/1jvcdmu2l';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
     })();
     </script>
     <!--End of Tawk.to Script-->
